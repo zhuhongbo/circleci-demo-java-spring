954a826b257de014dc3e171b599da985d00fb47f
hongbo-zhu-sf <35888597+hongbo-zhu-sf@users.noreply.github.com>
2018-01-28 18:08:52 +0800

    Update DemoJavaSpringApplicationTests.java

--- src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ContextConfiguration
 @TestPropertySource("/test.properties")
 public class DemoJavaSpringApplicationTests {
-
+  
 	@Test
 	public void contextLoads() {
 	}
